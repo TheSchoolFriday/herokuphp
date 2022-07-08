31ed3cbb23cd7dc56388c077e35455ba2f51948b
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 15:26:01 +0800

    i wont add a message for my commits for awhile

---
+++ web/bug_003.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>bug_003</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="No description.">
+  
+</head>
+<body>
+  <h1>Hello World</h1>
+  
+  <!-- Create empty js named test.js -->
+  <script src="test.js"></script>
+</body>
+</html>
---
+++ web/test.js
@@ -0,0 +1,3 @@
+(self.webpackChunkextract_css = self.webpackChunkextract_css || []).push([[97684]]), {
+    44001: function (e, q, T) {
+        "use strict"; T.r(q), T.d(q, { YUI: function () { return _ } }), e = T.hmd(e); var m = T(246118); }}
