28a0cf3763dc90b233688cef4a48b3386641328f
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 14:43:32 +0800

    Asf

---
+++ web/bug_001_fixed.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>bug_001_fixed</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="No description.">
+</head>
+<body>
+  <!-- Grab image from .assets/wallpaper.jpg -->
+  <img src=".assets/wallpaper.webp" alt="wallpaper" style="width: 100%; height: 100%;">
+  <img src=".assets/wallpaper.webp" alt="wallpaper" style="width: 100%; height: 100%;">
+  <img src=".assets/wallpaper.webp" alt="wallpaper" style="width: 100%; height: 100%;">
+  <img src=".assets/wallpaper.webp" alt="wallpaper" style="width: 100%; height: 100%;">
+  <img src=".assets/wallpaper.webp" alt="wallpaper" style="width: 100%; height: 100%;">
+
+</body>
+</html>
