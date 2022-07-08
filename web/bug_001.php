a123b870eb7f72bfd5c6c068ff05e7ba9550b24e
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 14:30:37 +0800

    Asf

--- web/bug_001.php
+++ web/bug_001.php
@@ -7,6 +7,6 @@
 </head>
 <body>
   <!-- Grab image from .assets/wallpaper.jpg -->
-  <img src=".assets/wallpaper.jpg" alt="wallpaper" style="width: 100%; height: 100%;">
+  <img src=".assets/wallpaper.jpg" alt="wallpaper" style="width: 500px; height: 500px;">
 </body>
 </html>
