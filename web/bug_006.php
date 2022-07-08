645f208b44560377f492c69de874d0cd4d410368
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 16:21:11 +0800

    i wont add a message for my commits for awhile

---
+++ web/bug_006.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>bug_006</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  
+</head>
+<body>
+
+  <h1>Testing</h1>
+
+</body>
+</html>
---
+++ web/bug_006_fixed.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>bug_006</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Add Meta -->
+  <meta name="description" content="No description.">
+  
+</head>
+<body>
+
+  <h1>Testing</h1>
+
+</body>
+</html>
