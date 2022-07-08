525793bdd85777f2bdf12c51734e64f2367fd5af
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 15:10:52 +0800

    i wont add a message for my commits for awhile

---
+++ web/bug_002_fixed.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>bug_002</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="No description.">
+  <!-- Remove empty css -->
+</head>
+<body>
+
+  <h1>Hello World</h1>
+
+</body>
+</html>
