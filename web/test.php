16e032aed8e32f2f4aed5cbc19542b7ed045db6d
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 12:15:03 +0800

    attempt for higher rate

--- web/test.php
+++ web/test.php
@@ -1,7 +1,15 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Fix: meta name -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Fix: Add Title -->
+    <title>Beautiful Title Nicely done</title>
 </head>
 <body>
-  Hello World!
+    <!-- "Welcome to the Test Website" in 24px font size -->
+    <h1 style="font-size: 24px;">Welcome to the Test Website</h1>
 </body>
 </html>
