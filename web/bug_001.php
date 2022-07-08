620093e7bc7f843236a4a8e5d7bc13e228abb672
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 14:17:34 +0800

    asf

---
+++ web/bug_001.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>bug_001</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="No description.">
+</head>
+<body>
+  <h1>Welcome to the Test Website</h1>
+</body>
+</html>
