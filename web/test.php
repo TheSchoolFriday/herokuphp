8d3e332fbc6796ccb4cdb36e0ed96f1bfd6f57bb
TheSchoolFriday <20046797@myrp.edu.sg>
2022-07-08 12:18:34 +0800

    test.php

--- web/test.php
+++ web/test.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Fix: meta name -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="testDescription" content="This is my test website in order to achieve a full 100 across all the spectrums in the lighthouse settings.">
     <!-- Fix: Add Title -->
     <title>Beautiful Title Nicely done</title>
 </head>
 <body>
-    <!-- "Welcome to the Test Website" in 24px font size -->
+    <!-- Fix: Change Font size to 24px -->
     <h1 style="font-size: 24px;">Welcome to the Test Website</h1>
 </body>
 </html>
